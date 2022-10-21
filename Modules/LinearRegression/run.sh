d0f458aeb05b713aa8538ceb882fa6a280766e1c
RishanthRajendhran <rishanthrajendhran@outlook.com>
2022-10-21 09:40:16 -0600

    Changed learning Rate in echo statements in LinearRegression/run.sh

--- Modules/LinearRegression/run.sh
+++ Modules/LinearRegression/run.sh
@@ -1,6 +1,6 @@
 echo "Building packages..."
 pip3 install -e ../../
-echo "Running Linear Regression (with Batch GD) over concrete dataset with a learning rate of 0.00001 in debug mode"
+echo "Running Linear Regression (with Batch GD) over concrete dataset with a learning rate of 0.00048828 in debug mode"
 python3 predict.py -ds concrete -debug -lr 0.00048828
-echo "Running Linear Regression (with Stochastic GD) over concrete dataset with a learning rate of 0.00001 in debug mode"
-python3 predict.py -ds concrete -debug -gd stochastic -lr 0.00048828
+echo "Running Linear Regression (with Stochastic GD) over concrete dataset with a learning rate of .0000048828 in debug mode"
+python3 predict.py -ds concrete -debug -gd stochastic -lr .0000048828
